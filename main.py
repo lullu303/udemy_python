25beaf6742c0c9c7a4bd680a0a6764f69a16d425
ji hye park <109124273+lullu303@users.noreply.github.com>
2023-01-04 11:34:30 +0000

    commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+#Write your code below this line 👇
+
+print("day 1 - Python Print Function")
+print("the function is declared like this")
+print("'what to print'")
+
+
+
+
+
+
+
+
+
