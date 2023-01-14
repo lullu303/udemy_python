836ce0c206cff192bed65da8e543520797e6d65c
ji hye park <109124273+lullu303@users.noreply.github.com>
2023-01-14 15:09:27 +0900

    Add files via upload

---
+++ day02/day-2-start.py
@@ -0,0 +1,22 @@
+#Data Types
+
+#String
+
+print("Hello"[0])
+
+print("Hello"[4])
+
+print("123"[0:3])
+
+print(123+345) #integer
+print("123"+"345")
+print("hello" + "world")
+
+print(123_456_789)
+
+#float
+print(3.141592)
+
+#boolean
+True
+False
