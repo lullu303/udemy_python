b09de2163b2c392699dd8758d6812bb6adb00927
ji hye park <109124273+lullu303@users.noreply.github.com>
2023-01-14 15:39:40 +0900

    Add files via upload

---
+++ day02/day-2-1-exercise.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# print(type(two_digit_number))
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+
+print(first_digit)
+print(second_digit)
+
+print(int(first_digit)+ int(second_digit))
+
+
+
+
